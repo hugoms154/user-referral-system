6a897b7f58fcc5d35248a675ce145982d0c8283c
hugo miranda <hugo.miranda.souza@gmail.com>
2023-08-06 19:28:22 -0300

    remove test query

--- api/src/modules/affiliated-link/affiliated-link.resolver.ts
+++ api/src/modules/affiliated-link/affiliated-link.resolver.ts
@@ -1,26 +1,25 @@
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Resolver } from "type-graphql";
 import { Service } from "typedi";
-import { AffiliatedLinkResponse, CreateAffiliatedLinkInput } from "./affiliated-link.graphql";
-import { AffiliatedLinkModel, CreateAffiliatedLinkModel } from "./affiliated-link.model";
+import {
+  AffiliatedLinkResponse,
+  CreateAffiliatedLinkInput,
+} from "./affiliated-link.graphql";
+import {
+  AffiliatedLinkModel,
+  CreateAffiliatedLinkModel,
+} from "./affiliated-link.model";
 import { CreateAffiliatedLinkUseCase } from "./create-affiliated-link.use-case";
-
 
 @Resolver()
 @Service()
 export class AffiliatedLinkResolver {
-
   constructor(private readonly createUseCase: CreateAffiliatedLinkUseCase) {}
 
   @Mutation(() => AffiliatedLinkResponse)
   createAffiliatedLink(
-    @Arg("data", () => CreateAffiliatedLinkInput) 
+    @Arg("data", () => CreateAffiliatedLinkInput)
     params: CreateAffiliatedLinkModel
   ): Promise<AffiliatedLinkModel> {
-    return this.createUseCase.exec(params)
-  }
-
-  @Query(() => String)
-  test() {
-    return "OK"
+    return this.createUseCase.exec(params);
   }
 }
